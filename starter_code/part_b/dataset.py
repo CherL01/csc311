6adee6da3e221c05a49b30aa2d41f132ccd202a1
Ryan Zhang <runze.zhang2001@gmail.com>
2023-03-30 13:30:40 -0400

    finished dataset class

--- starter_code/part_b/dataset.py
+++ starter_code/part_b/dataset.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,25 +43,38 @@ class StudentData(Dataset):
         self.zero_train_matrix = zero_train_matrix
         self.train_matrix = train_matrix
 
-        self.student_df = pd.read_csv("../data/student_meta.csv")
-        self.student_df
+        df = pd.read_csv(os.path.join(data_path, "student_meta.csv"), index_col="user_id")
+        df["data_of_birth"] = df["data_of_birth"].fillna(value="2005").apply(func=lambda x: int(x.split("-")[0]))
+        df["data_of_birth"] = 2020 - df["data_of_birth"]
 
+        df["premium_pupil"] = df["premium_pupil"].fillna(value=0.0).apply(func=lambda x: int(x))
+        df["gender"] = df["gender"].fillna(value=0).apply(func=lambda x: int(x))
 
+        self.student_df = df
+
+        self.normalize_meta_data()
 
     def __len__(self):
         return self.train_matrix.shape[0]
 
     def normalize_meta_data(self):
         for col in self.student_df.columns:
-            if col == "user_id":
-                self.student_df[col]
+            if col == "data_of_birth":
+                df = self.student_df[col]
+                self.student_df[col] = (df - np.mean(df)) / np.std(df)
+
+    def __getitem__(self, index) -> torch.Tensor:
+        
+        student_meta_data = torch.tensor(self.student_df.loc[[index], ["gender", "premium_pupil"]].to_numpy())
+        print(student_meta_data)
+        student_meta_data = F.one_hot(student_meta_data).reshape(-1)
+
+        student_meta_data = torch.concat((student_meta_data, torch.tensor(self.student_df.loc[[index], ["data_of_birth"]].to_numpy()).reshape(-1)))
+        print(student_meta_data)
+
+        return self.zero_train_matrix[index], self.train_matrix[index], student_meta_data
 
 if __name__ == "__main__":
-    df = pd.read_csv("../data/student_meta.csv", index_col="user_id")
-    df["data_of_birth"] = df["data_of_birth"].fillna(value="2005").apply(func=lambda x: int(x.split("-")[0]))
-    df["data_of_birth"] = 2020 - df["data_of_birth"]
 
-    df["premium_pupil"] = df["premium_pupil"].fillna(value=0.0).apply(func=lambda x: int(x))
-    df["gender"] = df["gender"].fillna(value=0).apply(func=lambda x: int(x))
-
-    print(df.head())
+    dataset = StudentData([0], [0])
+    print(dataset[0])
